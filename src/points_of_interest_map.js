c708775f521cd9e57888e49660f1925549818175
Michael Dailey <medaileyjr@gmail.com>
2019-10-31 10:50:37 -0400

    Updates from MAC

--- src/points_of_interest_map.js
+++ src/points_of_interest_map.js
@@ -21,6 +21,7 @@ function pointofinterest_and_map (id,indx) {
     var showChartOnPage = true;
     var CENTER_LOC = [];
     var SCENARIO_FOCUS = false;
+    var focusColor = "Yellow";
     var ROTATELABEL = 0;
     var BARSPACING = 0.2;
     var showCycleTools = true;
@@ -32,6 +33,8 @@ function pointofinterest_and_map (id,indx) {
     var circleMarkers;
     var groupColumn;
     var selectedGroup;
+    var scenarioPolyFile;
+    var highlightLayer;
     var selectedDataGrp;
     var palette = [["rgb(0, 0, 0)", "rgb(67, 67, 67)", "rgb(102, 102, 102)", "rgb(204, 204, 204)", "rgb(217, 217, 217)", "rgb(255, 255, 255)"], ["rgb(152, 0, 0)", "rgb(255, 0, 0)", "rgb(255, 153, 0)", "rgb(255, 255, 0)", "rgb(0, 255, 0)", "rgb(0, 255, 255)", "rgb(74, 134, 232)", "rgb(0, 0, 255)", "rgb(153, 0, 255)", "rgb(255, 0, 255)"], ["rgb(230, 184, 175)", "rgb(244, 204, 204)", "rgb(252, 229, 205)", "rgb(255, 242, 204)", "rgb(217, 234, 211)", "rgb(208, 224, 227)", "rgb(201, 218, 248)", "rgb(207, 226, 243)", "rgb(217, 210, 233)", "rgb(234, 209, 220)", "rgb(221, 126, 107)", "rgb(234, 153, 153)", "rgb(249, 203, 156)", "rgb(255, 229, 153)", "rgb(182, 215, 168)", "rgb(162, 196, 201)", "rgb(164, 194, 244)", "rgb(159, 197, 232)", "rgb(180, 167, 214)", "rgb(213, 166, 189)", "rgb(204, 65, 37)", "rgb(224, 102, 102)", "rgb(246, 178, 107)", "rgb(255, 217, 102)", "rgb(147, 196, 125)", "rgb(118, 165, 175)", "rgb(109, 158, 235)", "rgb(111, 168, 220)", "rgb(142, 124, 195)", "rgb(194, 123, 160)", "rgb(166, 28, 0)", "rgb(204, 0, 0)", "rgb(230, 145, 56)", "rgb(241, 194, 50)", "rgb(106, 168, 79)", "rgb(69, 129, 142)", "rgb(60, 120, 216)", "rgb(61, 133, 198)", "rgb(103, 78, 167)", "rgb(166, 77, 121)", "rgb(91, 15, 0)", "rgb(102, 0, 0)", "rgb(120, 63, 4)", "rgb(127, 96, 0)", "rgb(39, 78, 19)", "rgb(12, 52, 61)", "rgb(28, 69, 135)", "rgb(7, 55, 99)", "rgb(32, 18, 77)", "rgb(76, 17, 48)"]];
     var latColumn;
@@ -105,11 +108,17 @@ function pointofinterest_and_map (id,indx) {
                     }
                 });
                 var configSettings = data["POIMap"][configName];
+                //go through region level settings
                 $.each(data, function (key, val) {
                     if (key == "CenterMap")
                         CENTER_LOC = val;
                 });
-
+                //go through scenario config level settings
+                if (data["scenarios"][scenario]["ScenarioFocus"] != undefined) {
+                        SCENARIO_FOCUS = true;
+                        scenarioPolyFile = data["scenarios"][scenario]["ScenarioFocus"];
+                        $('#' + id + '-tooltable tbody tr').append("<td>Focus Color:</td> <td><input type='text' id='" + id + "-focus-color' style='display: none;' > </td> ");
+                    }
 
                 $.each(configSettings, function (opt, value) {
                     if (opt == "RotateLabels") {
@@ -119,12 +128,11 @@ function pointofinterest_and_map (id,indx) {
                         BARSPACING = value;
                     }
                     if (opt == "LegendTitle") {
-                        $('.legendtitle').html(value);
+                        $('#'+id+'-div .legendtitle').html(value);
                     }
                     if (opt == "CenterMap") {
                         CENTER_LOC = value;
                     }
-
                 })
 
             }).complete(function(){
@@ -354,7 +362,7 @@ function pointofinterest_and_map (id,indx) {
 
     function createMap(callback) {
 
-        if (map != undefined) {
+        if (map !== undefined) {
             return;
         }
         var tonerLayer = L.tileLayer('//stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.png', { id:id + "-by-district-map.toner",
@@ -384,8 +392,28 @@ function pointofinterest_and_map (id,indx) {
 
         }
         L.control.layers(baseMaps).addTo(map);
+
+         if (scenarioPolyFile != undefined) {
+            $.getJSON(dataLocation  + scenarioPolyFile, function (scenarioTiles) {
+                "use strict";
+                var focusLayer;
+                focusLayer = L.geoJSON(scenarioTiles, {
+                    style: styleFocusGeoJSONLayer
+                });
+                focusLayer.addTo(map);
+            });
+        }
         redrawMap();
 
+    }
+    function styleFocusGeoJSONLayer(feature) {
+        var returnStyle = {
+            //all SVG styles allowed
+            stroke: true,
+            weight: 5,
+            color: focusColor
+        };
+        return (returnStyle);
     }
 
     function redrawMap() {
@@ -518,6 +546,24 @@ function pointofinterest_and_map (id,indx) {
                 extNvd3Chart.groupSpacing(0.2);
             }
             extNvd3Chart.update();
+        });
+                $("#" + id + "-focus-color").spectrum({
+            color: focusColor,
+            showInput: true,
+            className: "full-spectrum",
+            showInitial: false,
+            showPalette: true,
+            showAlpha: true,
+            showSelectionPalette: true,
+            maxSelectionSize: 10,
+            preferredFormat: "hex",
+            localStorageKey: "spectrum.demo",
+            palette: palette,
+            change: function (color) {
+                focusColor = color;
+                redrawMap();
+
+            }
         });
         if(bubblesShowing){
             updateMapUI();
